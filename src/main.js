59b48c717a716cd31d116881953df7e771f5174e
razanjbara <127765142+razan2704@users.noreply.github.com>
2024-06-11 20:54:56 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -36,7 +36,32 @@ Vue.use(Vuelidate);
   ToastPlugin,
   LayoutPlugin,
 ].forEach((x) => Vue.use(x));
+Vue.use(Vuelidate);
 
+axios.interceptors.request.use(
+  function(config) {
+    // Do something before request is sent
+    return config;
+  },
+  function(error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+axios.interceptors.response.use(
+  function(response) {
+    // Do something with response data
+    return response;
+  },
+  function(error) {
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
+
+Vue.use(VueAxios, axios);
 const router = new VueRouter({
   routes,
 });
