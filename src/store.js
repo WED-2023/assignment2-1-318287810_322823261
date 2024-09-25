2431fd7b47a3310e0c57d1a4d6d3eb7a78377ecf
ShilatAlon <130230860+ShilatAlon@users.noreply.github.com>
2024-09-25 20:01:36 +0300

    Update store.js

--- src/store.js
+++ src/store.js
@@ -8,23 +8,39 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     isLoggedIn: false, // Track user login status
+    username: null,
   },
   mutations: {
     SET_LOGIN_STATUS(state, status) {
       state.isLoggedIn = status;
     },
+    SET_USERNAME(state, username) {
+      state.username = username; // שמירת שם המשתמש ב-Vuex
+    },
+    LOGOUT(state) {
+      state.isLoggedIn = false;
+      state.username = null;
+      localStorage.removeItem('loggedInUser');
+    },
   },
   actions: {
-    login({ commit }) {
+    login({ commit }, username) {
       // Simulate successful login
-      commit('SET_LOGIN_STATUS', true);
+      commit('SET_LOGIN_STATUS', { status: true, username });
+      commit('SET_USERNAME', { status: true, username });
+      localStorage.setItem('loggedInUser', username);
     },
     logout({ commit }) {
-      // Simulate logout
-      commit('SET_LOGIN_STATUS', false);
+      console.log("logout from Vuex store");
+      localStorage.removeItem('username');
+      localStorage.removeItem('lastSearchQuery');
+      commit('LOGOUT');
     },
   },
   getters: {
     isLoggedIn: state => state.isLoggedIn,
+    username: state => state.username,
   },
 });
+
+
