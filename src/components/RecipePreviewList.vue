eb4a2154dea4731a19d54440221a2d2e1a67b1ff
ShilatAlon <130230860+ShilatAlon@users.noreply.github.com>
2024-09-25 19:46:42 +0300

    Update RecipePreviewList.vue

--- src/components/RecipePreviewList.vue
+++ src/components/RecipePreviewList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="recipe-preview-list">
     <h2>{{ title }}</h2>
-    <div v-if="recipes.length">
+    <div v-if="recipes && recipes.length">
       <RecipePreview
         v-for="recipe in recipes"
         :key="recipe.id"
@@ -16,7 +16,9 @@
 
 <script>
 import RecipePreview from './RecipePreview.vue';
-import { mockGetRecipesPreview, mockGetRecipeFullDetails } from "../services/recipes.js";
+//import { mockGetRecipesPreview, mockGetRecipeFullDetails } from "../services/recipes.js";
+import { getRecipesPreview, getRecipeFullDetails } from "../services/recipes.js";
+
 
 export default {
   components: {
@@ -27,36 +29,38 @@ export default {
       type: String,
       required: true
     },
-    recipeIds: {
+    recipes: {
       type: Array,
       default: () => []
     }
   },
-  data() {
-    return {
-      recipes: []
-    };
-  },
-  async mounted() {
-    await this.loadRecipes();
-  },
   methods: {
-    async loadRecipes() {
-      if (this.recipeIds.length) {
-        for (let recipeId of this.recipeIds) {
-          const response = mockGetRecipeFullDetails(recipeId);
-          if (response.data && response.data.recipe) {
-            this.recipes.push(response.data.recipe);
-          }
-        }
-      } else {
-        const response = await mockGetRecipesPreview(3);
-        if (response.data && response.data.recipes) {
-          this.recipes = response.data.recipes;
-        }
-      }
-    },
+    // async loadRecipes() {
+    //   if (this.recipeIds.length) {
+    //     for (let recipeId of this.recipeIds) {
+    //       try {
+    //         const recipeDetails = await getRecipeFullDetails(recipeId); // Corrected function call
+    //         if (recipeDetails) {
+    //           this.recipes.push(recipeDetails); // Adjusted to handle the API response
+    //         }
+    //       } catch (error) {
+    //         console.error("Error fetching recipe details:", error);
+    //       }
+    //     }
+    //   } else {
+    //     try {
+    //       const recipesPreview = await getRecipesPreview(this.recipeIds); // Corrected function call
+    //       if (recipesPreview) {
+    //         this.recipes = recipesPreview;
+    //       }
+    //     } catch (error) {
+    //       console.error("Error fetching recipes preview:", error);
+    //     }
+    //   }
+    // },
     hasViewed(recipeId) {
+      console.log('Recipes passed to RecipePreviewList:', this.recipes);
+
       let viewedRecipes = JSON.parse(localStorage.getItem('viewedRecipes')) || [];
       return viewedRecipes.includes(recipeId);
     },
